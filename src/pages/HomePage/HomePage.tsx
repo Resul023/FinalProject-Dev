e62594af059baea297ba996eac7e9dedc5311ab5
Resul023 <73m6u7h@code.edu.az>
2022-12-04 18:30:12 +0400

    Created Deliver and Trial diet sections

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,8 @@ import "./HomePage.scss";
 import img from "./Diets/images/imageFood.png";
 import { Diets } from "./Diets/Diets";
 import { IDiet } from "../../models/Diet";
+import { Deliver } from "./Deliver/Deliver";
+import { TrialDiet } from "./TrialDiet/TrialDiet";
 export const HomePage = () => {
   const dietItems: IDiet[] = [
     {
@@ -78,6 +80,14 @@ export const HomePage = () => {
   return (
     <>
       <Diets title="The most popular diets" dietItems={dietItems} />
+      <Deliver
+        contentText="See if we're delivering to you right now! Use the search engine by
+              entering the postal code of your locality and see if we can
+              deliver your order there."
+        buttonText="Check"
+        iconText="Locate me"
+      />
+      <TrialDiet />
     </>
   );
 };
