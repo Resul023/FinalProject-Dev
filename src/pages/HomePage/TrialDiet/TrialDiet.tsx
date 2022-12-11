469dd7f247937150cfd8beb2bcdaf0db18a247f5
Resul023 <73m6u7h@code.edu.az>
2022-12-11 15:24:23 +0400

    create menu page and updated figure component

--- src/pages/HomePage/TrialDiet/TrialDiet.tsx
+++ src/pages/HomePage/TrialDiet/TrialDiet.tsx
@@ -4,16 +4,14 @@ import rightTopFigure from "./Vector 17.svg";
 import fruitLeft from "./fruit-left.png";
 import fruitRight from "./fruit-right.png";
 import shape from "./Subtract.svg";
+import { Figure } from "../../../components/Figure/Figure";
 export const TrialDiet = () => {
   return (
     <section id="trial-diet">
       <div className="container">
-        <div className="container__top-left">
-          <img src={leftTopFigure} alt="" />
-        </div>
-        <div className="container__top-right">
-          <img src={rightTopFigure} alt="" />
-        </div>
+        <Figure className="container__top-left" />
+        <Figure className="container__top-right" />
+
         <div className="container-content">
           <div className="container__left-content">
             <img className="left-shape" src={shape} alt="" />
@@ -29,13 +27,8 @@ export const TrialDiet = () => {
             <img className="fruit-right" src={fruitRight} alt="" />
           </div>
         </div>
-
-        <div className="container__bottom-left">
-          <img src={leftTopFigure} alt="" />
-        </div>
-        <div className="container__bottom-right">
-          <img src={rightTopFigure} alt="" />
-        </div>
+        <Figure className="container__bottom-left" />
+        <Figure className="container__bottom-right" />
       </div>
     </section>
   );
--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -1,4 +1,120 @@
+import { CategoryBox } from "../../components/CategoryBox/CategoryBox";
+import { Title } from "../../components/Title/Title";
+import Select from "react-select";
 import "./MenuPage.scss";
+import { DishBox } from "../../components/DishBox/DishBox";
+
+const options = [
+  { value: "meal", label: "meal" },
+  { value: "meal", label: "meal" },
+  { value: "meal", label: "meal" },
+];
+const customStyles = {
+  control: (base: any) => ({
+    ...base,
+    height: 50,
+    minHeight: 50,
+    paddingTop: 7,
+    paddingBottom: 7,
+  }),
+};
+
 export const MenuPage = () => {
-  return <h1>MenuPage</h1>;
+  return (
+    <section id="menu">
+      <div className="container">
+        <Title title="Menu" />
+        <div className="menu-category">
+          <div className="category-boxes">
+            <CategoryBox categoryName="Text" />
+            <CategoryBox categoryName="Text" />
+            <CategoryBox categoryName="Text" />
+            <CategoryBox categoryName="Text" />
+            <CategoryBox categoryName="Text" />
+            <CategoryBox categoryName="Text" />
+          </div>
+          <div className="category-select">
+            <p>Choose a diet:</p>
+            <Select
+              className="select-input"
+              options={options}
+              isClearable={true}
+              isSearchable={true}
+              isMulti
+              styles={customStyles}
+              classNamePrefix="mySelect"
+            />
+          </div>
+        </div>
+        <div className="menu-products">
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+          <DishBox
+            rate={4.8}
+            title="Molestiae sed augue eu sapien hendrerit fringilla"
+            text="Taque earum rerum hic tenetur a sapiente"
+            calories={420}
+            dietTypeOne="Diet:"
+            dietTypeTwo="Slim"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
